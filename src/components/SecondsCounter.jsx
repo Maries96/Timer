33a0cb39b43fdec92c96b7384862bf741b2e9cac
Marie <marievstewart@gmail.com>
2023-03-07 01:14:33 +0000

    Timer Project

---
+++ src/components/SecondsCounter.jsx
@@ -0,0 +1,23 @@
+import  { withTimer } from "@/lib/withTimer";
+
+export function SecondsCounter ({ seconds }) {
+
+const secondsInString = seconds.toString();
+const paddedStart = secondsInString.padStart(6,"0");
+const digits = paddedStart.split("");
+
+  return ( 
+  <div className="row">
+    {digits.map((digit, index) => {
+        return (
+            <div
+                key={index}
+                className= "col text-xl"
+                style={{ padding: 20, border: "solid 1px black"}}>
+                    <h1>{digit}</h1>
+            </div>
+        );
+    })}</div>
+  );
+}
+export const SecondsCounterWithTimer = withTimer(SecondsCounter);
